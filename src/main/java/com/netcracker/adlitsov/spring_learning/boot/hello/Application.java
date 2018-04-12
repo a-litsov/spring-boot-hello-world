2b88847d487d3b0dc08bc2ae22d80acd37351850
alex <adlitsov@gmail.com>
2018-04-12 15:41:10 +0300

    pom.xml: changed groupId and renamed package

--- src/main/java/com/netcracker/adlitsov/spring_learning/boot/hello/Application.java
+++ src/main/java/com/netcracker/adlitsov/spring_learning/boot/hello/Application.java
@@ -1,4 +1,4 @@
-package hello;
+package com.netcracker.adlitsov.spring_learning.boot.hello;
 
 import java.util.Arrays;
 
--- src/main/java/com/netcracker/adlitsov/spring_learning/boot/hello/HelloController.java
+++ src/main/java/com/netcracker/adlitsov/spring_learning/boot/hello/HelloController.java
@@ -1,4 +1,4 @@
-package hello;
+package com.netcracker.adlitsov.spring_learning.boot.hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
